13c1bbcd9b23afc00870c4e8364b1e35eae0d22a
akashkaturi <64057380+akashkaturi@users.noreply.github.com>
2021-06-28 12:20:32 +0530

    Create Place_at_a_position.py

---
+++ Place_at_a_position.py
@@ -0,0 +1,11 @@
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+variables = input("Where do you want to put the treasure?(Row,column)(seperated by commas(,)) ")
+position=variables.split(',')
+vertical=int(position[1])
+horizontal=int(position[0])
+map[vertical-1][horizontal-1]="🚨"
+print(f"\n\n\n{row1}\n{row2}\n{row3}")
