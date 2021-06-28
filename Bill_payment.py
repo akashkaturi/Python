fcbc76ed25911c34d192f7370b922d3743f38622
akashkaturi <64057380+akashkaturi@users.noreply.github.com>
2021-06-28 12:15:08 +0530

    Create Bill_payment.py

---
+++ Bill_payment.py
@@ -0,0 +1,11 @@
+import random
+names_csv=input("Enter names who are ready to participate!(seperated by commas)")
+names=names_csv.split(",")
+length=len(names)
+
+number=input("Create a seed number")
+manipulation=random.randint(1,100)
+f_number=int(manipulation)-int(number)
+random.seed(f_number)
+k=random.choice(names) 
+print(k,"has to pay the bill")
